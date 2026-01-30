cba184bac05cfbb195241c50b938be9bf219042c
Terry Qiu <terryq2@illinois.edu>
2026-01-30 11:51:43 -0600

    path fix

--- terry_algo1/helpers.py
+++ terry_algo1/helpers.py
@@ -1,10 +1,11 @@
 import classes
 import random
-
-print("hello world")
+from pathlib import Path
 
 def generate_n_tiles_at_random(n: int):
-    with open('input_tileset.xml', 'w') as file:
+    print(Path(__file__).parent)
+    file_path = Path(__file__).parent / 'input_tileset.xml'
+    with open(file_path, 'w') as file:
         for i in range(n):
             N = random.randint(1, 4)
             E = random.randint(1, 4)
--- terry_algo1/main.py
+++ terry_algo1/main.py
@@ -5,13 +5,11 @@ import helpers
 
 if __name__ == "__main__":
     tiles = []
-    helpers.generate_n_tiles_at_random(5)
+    helpers.generate_n_tiles_at_random(10)
     with open('input_tileset.xml', 'r') as file:
         content = file.read().split('\n')
         for line in content:
             sides = line.split(' ')
             tiles.append(classes.Tile(int(sides[0][1]), int(sides[1][1]), int(sides[1][1]), int(sides[1][1])))
-
-    
 
     
