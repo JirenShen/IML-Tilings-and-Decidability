60a9cb7835278eb281036f722be3b509542b7719
maxwellhhong <maxwell.hong.2015@gmail.com>
2026-02-03 16:23:25 -0600

    changed to only expand through Northwest corner

--- code-for-prop-6.1.34/tiles.py
+++ code-for-prop-6.1.34/tiles.py
@@ -32,15 +32,12 @@ def parity(n) -> int:
         return 1
     else:
         return -1
-    
-def shiftIdxOneToZero(n):
-    return n - int(n>0)
 
 # tileset is a list of tiles
 class TilingSquare:
     def __init__(self):
         self.square_sides = ([], [], [], []) # respective color sequences of north, east, south, and west sides
-        self.num_missing_from_each_side = [0, 0, 0, 0] # denotes num of tiles missing from each side, pos/neg indicates missing from which end
+        self.num_missing_from_each_side = [0, 0] # denotes num of tiles missing from north and west sides respectively, pos/neg indicates missing from which end
 
     def __str__(self):
         return str(self.square_sides) + ", " + str(self.num_missing_from_each_side)
@@ -61,32 +58,6 @@ class TilingSquare:
         return (self.square_sides[Sides.North.value] == self.square_sides[Sides.South.value]
                 and self.square_sides[Sides.East.value] == self.square_sides[Sides.West.value])
 
-    # returns None if cannot add tile, otherwise returns tiled_square with new tile
-    # (sorry for weird architecture of splitting between object methods and external methods)
-    def squareWithFillingTile(self, tile):
-        curr_side = self.firstMissingSide()
-        # has no missing tiles, should not try adding tile in first place
-        if curr_side is None: return None
-        
-        # if positive, shift to 0-index
-        nth_missing = self.num_missing_from_each_side[curr_side]
-        curr_idx = nth_missing - int(nth_missing > 0)
-        
-        prev_tile_interfacing_side = ((curr_side % 2) + 2*int(curr_idx < 0) - 1) % NUM_OF_SIDES
-        prev_tile_interfacing_idx = 0
-        if curr_side == 0 or curr_side == 1:
-            prev_tile_interfacing_idx = len(self.square_sides[curr_side]) - 1
-
-        if (tile[oppSide(curr_side)] == self.square_sides[curr_side][curr_idx]
-                and tile[oppSide(prev_tile_interfacing_side)] == self.square_sides[prev_tile_interfacing_side][prev_tile_interfacing_idx]):
-            new_sq = copy.deepcopy(self)
-            new_sq.square_sides[curr_side][curr_idx] = tile[curr_side]
-            new_sq.square_sides[prev_tile_interfacing_side][prev_tile_interfacing_idx] = tile[prev_tile_interfacing_side]
-            new_sq.num_missing_from_each_side[curr_side] -= parity(nth_missing)
-            return new_sq
-        else:
-            return None
-
     # finds arbitrary side with missing tiles
     def firstMissingSide(self):
         for i in range(len(self.num_missing_from_each_side)):
@@ -95,26 +66,22 @@ class TilingSquare:
         return None
 
 
+# returns None if cannot add tile, otherwise returns tiled_square with new tile
 def squaresWithTileInserted(sqr, tile):
     curr_side = sqr.firstMissingSide()
     # has no missing tiles, should not try adding tile in first place
     if curr_side is None: return None
     
-    # if positive, shift to 0-index
-    nth_missing = sqr.num_missing_from_each_side[curr_side]
-    curr_idx = nth_missing - int(nth_missing > 0)
-    
-    prev_tile_interfacing_side = ((curr_side % 2) + 2*int(curr_idx < 0) - 1) % NUM_OF_SIDES
-    prev_tile_interfacing_idx = 0
-    if curr_side == 0 or curr_side == 1:
-        prev_tile_interfacing_idx = len(sqr.square_sides[curr_side]) - 1
+    curr_idx = sqr.num_missing_from_each_side[curr_side] - 1
+    prev_tile_interfacing_side = 1 - curr_side
+    prev_tile_interfacing_idx = len(sqr.square_sides[curr_side]) - 1
 
     if (tile[oppSide(curr_side)] == sqr.square_sides[curr_side][curr_idx]
             and tile[oppSide(prev_tile_interfacing_side)] == sqr.square_sides[prev_tile_interfacing_side][prev_tile_interfacing_idx]):
         new_sq = copy.deepcopy(sqr)
         new_sq.square_sides[curr_side][curr_idx] = tile[curr_side]
         new_sq.square_sides[prev_tile_interfacing_side][prev_tile_interfacing_idx] = tile[prev_tile_interfacing_side]
-        new_sq.num_missing_from_each_side[curr_side] -= parity(nth_missing)
+        new_sq.num_missing_from_each_side[curr_side] -= 1
         return new_sq
     else:
         return None
@@ -147,7 +114,6 @@ class Tilings:
                 continue
 
             for tile in self.tiles:
-                # filled_sq = curr_sq.squareWithFillingTile(tile)
                 filled_sq = squaresWithTileInserted(curr_sq, tile)
 
                 if filled_sq is None or filled_sq in seen_solns: # ignores invalid tiling or previous solution
@@ -169,31 +135,16 @@ class Tilings:
     def putSquareWithAddedCornerTiles(self, square, tiles, worklist):
         if not square.firstMissingSide() is None: return # square not tiled
 
-        corners = ((Sides.North, Sides.East), (Sides.South, Sides.East), (Sides.South, Sides.West), (Sides.North, Sides.West))
-        for corner_dir1, corner_dir2 in corners:
-            corner_side1 = corner_dir1.value
-            corner_side2 = corner_dir2.value
-            old_sqr_size = len(square.square_sides[0])
-            
-            for tile in tiles:
-                new_sq = copy.deepcopy(square)
-                if corner_side2 < 2:
-                    new_sq.square_sides[corner_side1].append(tile[corner_side1])
-                    new_sq.square_sides[oppSide(corner_side1)].append(tile[oppSide(corner_side1)])
-                    new_sq.num_missing_from_each_side[corner_side1] = old_sqr_size
-                else:
-                    new_sq.square_sides[corner_side1].insert(0, tile[corner_side1])
-                    new_sq.square_sides[oppSide(corner_side1)].insert(0, tile[oppSide(corner_side1)])
-                    new_sq.num_missing_from_each_side[corner_side1] = -old_sqr_size
+        # only one corner is necessary, so doing Northeast
+        old_sqr_size = len(square.square_sides[0])
+        for tile in tiles:
+            new_sq = copy.deepcopy(square)
 
-                if corner_side1 < 2:
-                    new_sq.square_sides[corner_side2].append(tile[corner_side2])
-                    new_sq.square_sides[oppSide(corner_side2)].append(tile[oppSide(corner_side2)])
-                    new_sq.num_missing_from_each_side[corner_side2] = old_sqr_size
-                else:
-                    new_sq.square_sides[corner_side2].insert(0, tile[corner_side2])
-                    new_sq.square_sides[oppSide(corner_side2)].insert(0, tile[oppSide(corner_side2)])
-                    new_sq.num_missing_from_each_side[corner_side2] = -old_sqr_size
+            for dir in range(NUM_OF_SIDES):
+                new_sq.square_sides[dir].append(tile[dir])
 
-                worklist.put(new_sq)
+            new_sq.num_missing_from_each_side[0] = old_sqr_size
+            new_sq.num_missing_from_each_side[1] = old_sqr_size
+
+            worklist.put(new_sq)
     
