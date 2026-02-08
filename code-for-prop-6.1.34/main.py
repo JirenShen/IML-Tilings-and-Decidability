c9e59edc3d578dacf795b339908292d22cde2507
maxwellhhong <maxwell.hong.2015@gmail.com>
2026-02-07 18:18:59 -0600

    changed testing tileset to known 11-tile 4-color aperiodic tile for runtime testing

--- code-for-prop-6.1.34/main.py
+++ code-for-prop-6.1.34/main.py
@@ -3,7 +3,21 @@ import tiles
 if __name__ == "__main__":
     instructions = "Input next to view square tilings of n+1 size found by algorithm."
 
-    tileset = [(1, 1, 1, 1), (2, 2, 2, 2), (1, 2, 1, 2)]
+    # 11-tile 4-color aperiodic tile set of Jendel-Rao;
+    # Reference: https://amathr.org/an-aperiodic-set-of-eleven-wang-tiles/
+    tileset = [
+        (1, 2, 3, 2),
+        (4, 4, 1, 2),
+        (2, 3, 2, 4),
+        (3, 1, 2, 1),
+        (1, 1, 3, 1),
+        (3, 3, 2, 3), 
+        (2, 4, 1, 3),
+        (1, 3, 1, 2),
+        (2, 2, 2, 4),
+        (1, 2, 1, 4),
+        (2, 4, 4, 4)
+        ]
     tilings = tiles.Tilings()
     tilings.tiles = tileset
 
